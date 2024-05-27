28db74e52c8699247b94766f74b480256fe1c62d
smrat <smratxyt9old@gmail.com>
2024-05-27 22:26:33 +0400

    first commit

---
+++ Задание1/app.java
@@ -0,0 +1,10 @@
+int a = 3;
+int b = 6;
+
+if (a > b) {
+    System.out.print("а больше b");
+} else if (a < b) {
+    System.out.print("b больше a");
+} else {
+    System.out.print("a и b равны");
+}
---
+++ Задание2/app.java
@@ -0,0 +1,8 @@
+String str1 = new String("Flower");
+String str1 = new String("flower");
+
+if (str1.equals(snr2)) {
+    System.out.print("Строки равны");
+} else {
+    System.out.print("Строки не равны");
+}
