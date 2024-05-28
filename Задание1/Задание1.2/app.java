110fc6db0a3e6ce2aaeeebdcf92f68c5d867b894
smrat <smratxyt9old@gmail.com>
2024-05-28 14:02:08 +0400

    изменения`

--- Задание1/Задание1.2/app.java
+++ Задание1/Задание1.2/app.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String [] args) {
+        
         int a = 15;
         int b = 45;
+
         System.out.println(a+b);
         System.out.println(a/b);
         System.out.println(a*b);
